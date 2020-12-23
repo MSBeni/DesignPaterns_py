a582379af846105a3c356399fe2a5e96331ba16e
MSBeni <andrei.sokurov.bitco@gmail.com>
2020-12-23 17:55:14 -0500

    Added basic files 4

--- 1-python_essentials/Basics_enumerate.py
+++ 1-python_essentials/Basics_enumerate.py
@@ -13,8 +13,17 @@ for index_, c in enumerate(lst_):
 
 
 # ------------------------------------------------------------
-lambda_test = lambda x, *args, y, **kwargs: (x+10, args, y+5, kwargs)
+lam_test = lambda x, *args, y, **kwargs: (x+10, args, y+5, kwargs)
 
-print(lambda_test(1, 'a', 12, 24, a=50, y=20, b='sss'))
+print(lam_test(1, 'a', 12, 24, a=50, y=20, b='sss'))
 print('\n')
-print(lambda_test(1, 'a', 12, y=24, a=50, c=20, b='sss'))
+print(lam_test(1, 'a', 12, y=24, a=50, c=20, b='sss'))
+print('\n')
+
+
+# ------------------------------------------------------------
+def apply_func(fn, *args, **kwargs):
+    return fn(*args, **kwargs)
+
+
+print(apply_func(lambda x, y: x**y, 10, 20))
