251d290d8d06852241c022e57d7dad9c6ffa85da
MSBeni <andrei.sokurov.bitco@gmail.com>
2020-12-23 17:49:42 -0500

    Added basic files 3

--- 1-python_essentials/Basics_enumerate.py
+++ 1-python_essentials/Basics_enumerate.py
@@ -10,3 +10,11 @@ lst_ = ['a', 'b', 'c']
 
 for index_, c in enumerate(lst_):
     print(index_, c)
+
+
+# ------------------------------------------------------------
+lambda_test = lambda x, *args, y, **kwargs: (x+10, args, y+5, kwargs)
+
+print(lambda_test(1, 'a', 12, 24, a=50, y=20, b='sss'))
+print('\n')
+print(lambda_test(1, 'a', 12, y=24, a=50, c=20, b='sss'))
