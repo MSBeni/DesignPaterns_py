ba2be5c75de277e9a8213d9908a59c938cfa4a42
MSBeni <andrei.sokurov.bitco@gmail.com>
2020-12-31 16:23:28 -0500

    Added decorator 2

--- 1-python_essentials/cached_decorator_application.py
+++ 1-python_essentials/cached_decorator_application.py
@@ -22,3 +22,20 @@ print(fib_rec(8))
 
 print(fib_rec(30))
 print(fib_rec(40))
+print('\n')
+########################################################################################################################
+########################################################################################################################
+# We can also make use of lru_cache
+from functools import lru_cache
+
+
+# without indicating the maxsize it will be 128, if we put it None it will be unlimited, you can out also other values
+@lru_cache()
+def fib_rec(n):
+    print("Calculating fib({}):".format(n))
+    return 1 if n < 3 else fib_rec(n-2)+fib_rec(n-1)
+
+
+print(fib_rec(10))
+
+print(fib_rec(15))
